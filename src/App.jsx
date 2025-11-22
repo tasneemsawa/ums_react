d94d3e43662e947290e706e3e74e351a27ba6a3b
Tasneem Sawalha <tasneem31997@gmail.com>
2025-11-22 17:02:39 +0200

    add spa

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
+import Register from './Pages/Register/Register'
+import User from './Pages/User/User'
+import Home from './Pages/Home/Home'
+import Users from './Pages/User/Users'
+import AddUser from './Pages/User/AddUser'
+import NavBar from './components/NavBar/NavBar'
+import Footer from './components/Footer/Footer'
 
 function App() {
 
   return (
-    <div>
-      app
-    </div>
+<>
+      <NavBar />
+      <Routes>
+        <Route path='/home' element={<Home />}></Route>
+        <Route path='/user' element={<User />}></Route>
+        <Route path='/register' element={<Register />}></Route>
+        <Route path='/users' element={<Users />}></Route>
+        <Route path='/addUser' element={<AddUser />}></Route>
+
+        
+      </Routes>
+      <Footer />
+    </>
  
   )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function NavBar() {
   return (
-    <div>NavBar</div>
+ <>
+<nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <Link className="navbar-brand" href="#">Navbar</Link>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" to={"/home"}>Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to={"/user"}>Link</Link>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
+
+</>
   )
 }
